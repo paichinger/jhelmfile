2551779605447dfada8c2f0f6c61914f113d0aa4
Peter Aichinger <petera@topdesk.com>
2023-07-04 15:53:17 +0200

    remove unused imports.

--- src/main/java/com/paichinger/helmfile/models/build/Release.java
+++ src/main/java/com/paichinger/helmfile/models/build/Release.java
@@ -1,8 +1,6 @@
 package com.paichinger.helmfile.models.build;
 
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
